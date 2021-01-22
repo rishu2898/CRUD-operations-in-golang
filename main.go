c87533e89c30104b7643512c3b2580b47f448ad1
rishav2898 <rishav2898@gmail.com>
2021-01-22 13:05:28 +0530

    modified database connectivity

--- main.go
+++ main.go
@@ -18,32 +18,22 @@ type Employee struct {
 	Role   int    `json:"role"`
 }
 
-// variable for holding the database
-var DB *sql.DB
+type EmployeeHandler struct {
+	DB *sql.DB
+}
 
 // function to create the table into database
-func CreateTable() {
+func (empHandler *EmployeeHandler) CreateTable() {
 	query := `CREATE TABLE article (
 		id int,
 		title varchar(50),
 		desc varchar(50),
 		Content varchar(50)
 	)`
-	_, err := DB.Query(query)
+	_, err := empHandler.DB.Query(query)
 	if err != nil {
 		fmt.Println("tale not created")
 	}
-}
-
-// function to connect the database
-func ConnectDB() {
-	db, err := sql.Open("mysql", "rishabh:Rishu2898@@(127.0.0.1)/company")
-	if err != nil {
-		panic(err.Error())
-	}
-	DB = db
-	//defer DB.Close()
-	//CreateTable()
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -52,9 +42,9 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 // function to store the data from database into slice
-func storeRecord() []Employee {
+func (empHandler *EmployeeHandler) storeRecord() []Employee {
 	var emp []Employee
-	dis, err := DB.Query("select id, name, age, gender, role from employee")
+	dis, err := empHandler.DB.Query("select id, name, age, gender, role from employee")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -70,9 +60,9 @@ func storeRecord() []Employee {
 }
 
 // function for find all record
-func returnAllEmployees(w http.ResponseWriter, r *http.Request) {
+func (empHandler *EmployeeHandler) returnAllEmployees(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllArticles")
-	emp := storeRecord()
+	emp := empHandler.storeRecord()
 	data, err := json.Marshal(emp)
 	if err != nil {
 		panic(err)
@@ -81,11 +71,11 @@ func returnAllEmployees(w http.ResponseWriter, r *http.Request) {
 }
 
 // function for return particular single record
-func returnSingleEmployee(w http.ResponseWriter, r *http.Request) {
+func (empHandler *EmployeeHandler) returnSingleEmployee(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		vars := mux.Vars(r)
 		key := vars["id"]
-		res, _ := DB.Query(fmt.Sprintf("SELECT * FROM employee WHERE id=%v", key))
+		res, _ := empHandler.DB.Query(fmt.Sprintf("SELECT * FROM employee WHERE id=%v", key))
 
 		var emp []Employee
 		for res.Next() {
@@ -105,14 +95,14 @@ func returnSingleEmployee(w http.ResponseWriter, r *http.Request) {
 }
 
 // function for insert record
-func InsertRecord(w http.ResponseWriter, r *http.Request) {
+func (empHandler *EmployeeHandler) InsertRecord(w http.ResponseWriter, r *http.Request) {
 	var emp Employee
 	if r.Method == "POST" {
 		err := json.NewDecoder(r.Body).Decode(&emp)
 		if err != nil {
 			panic(err)
 		}
-		res, err := DB.Query(fmt.Sprintf("INSERT INTO employee(name, age, gender, role) VALUES('%v', %v, '%v', '%v')", emp.Name, emp.Age, emp.Gender, emp.Role))
+		res, err := empHandler.DB.Query(fmt.Sprintf("INSERT INTO employee(name, age, gender, role) VALUES('%v', %v, '%v', '%v')", emp.Name, emp.Age, emp.Gender, emp.Role))
 		if err != nil {
 			panic(err.Error())
 		}
@@ -123,7 +113,7 @@ func InsertRecord(w http.ResponseWriter, r *http.Request) {
 }
 
 // function for update single record
-func UpdateSingleRecord(w http.ResponseWriter, r *http.Request) {
+func (empHandler *EmployeeHandler) UpdateSingleRecord(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "PUT" {
 		vars := mux.Vars(r)
 		key := vars["id"]
@@ -136,7 +126,7 @@ func UpdateSingleRecord(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err)
 		}
-		res, _ := DB.Exec(fmt.Sprintf("UPDATE employee SET name = '%v', age = %v, gender = '%v', role = '%v' where id = %v", emp.Name, emp.Age, emp.Gender, emp.Role, key))
+		res, _ := empHandler.DB.Exec(fmt.Sprintf("UPDATE employee SET name = '%v', age = %v, gender = '%v', role = '%v' where id = %v", emp.Name, emp.Age, emp.Gender, emp.Role, key))
 		// return the number of rows affected by query
 		count, _ := res.RowsAffected()
 		if count == 0 {
@@ -147,12 +137,12 @@ func UpdateSingleRecord(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func DeleteSingleRecord(w http.ResponseWriter, r *http.Request) {
+func (empHandler *EmployeeHandler) DeleteSingleRecord(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "DELETE" {
 		vars := mux.Vars(r)
 		key := vars["id"]
-
-		res, _ := DB.Exec(fmt.Sprintf("DELETE FROM employee WHERE id = %v", key))
+		fmt.Println(key)
+		res, _ := empHandler.DB.Exec(fmt.Sprintf("DELETE FROM employee WHERE id = %v", key))
 		// return the number of rows affected by query
 		count, _ := res.RowsAffected()
 		if count == 0 {
@@ -164,18 +154,23 @@ func DeleteSingleRecord(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
+	// connecting to the database
+	db, err := sql.Open("mysql", "rishabh:Rishu2898@@(127.0.0.1)/company")
+	if err != nil {
+		panic(err.Error())
+	}
+	empHandler := &EmployeeHandler{DB: db}
+
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/employee", returnAllEmployees).Methods("GET")
-	myRouter.HandleFunc("/employee/{id}", returnSingleEmployee).Methods("GET")
-	myRouter.HandleFunc("/employee", InsertRecord).Methods("POST")
-	myRouter.HandleFunc("/employee/{id}", UpdateSingleRecord).Methods("PUT")
-	myRouter.HandleFunc("/employee/{id}", DeleteSingleRecord).Methods("DELETE")
+	myRouter.HandleFunc("/employee", empHandler.returnAllEmployees).Methods("GET")
+	myRouter.HandleFunc("/employee/{id}", empHandler.returnSingleEmployee).Methods("GET")
+	myRouter.HandleFunc("/employee", empHandler.InsertRecord).Methods("POST")
+	myRouter.HandleFunc("/employee/{id}", empHandler.UpdateSingleRecord).Methods("PUT")
+	myRouter.HandleFunc("/employee/{id}", empHandler.DeleteSingleRecord).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
 func main() {
-	ConnectDB()
 	handleRequests()
-	defer DB.Close()
 }
